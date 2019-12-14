1d9a45b52a7333696eb2744374e39b067649ca24
xiaohaozi9825 <1824465168@qq.com>
2019-12-14 17:51:28 +0800

    增加简单用法相关数据和界面，并测试通过

--- app/src/main/java/com/iflytek/myapplication/MainActivity.java
+++ app/src/main/java/com/iflytek/myapplication/MainActivity.java
@@ -2,14 +2,34 @@ package com.iflytek.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mBtnSimple;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnSimple = findViewById(R.id.btn_simple);
+        mBtnSimple.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_simple:
+                startActivity(new Intent(this, SimpleActivity.class));
+                break;
+//            case :
+//
+//                break;
+            default:
+                break;
+        }
     }
 }
